490edef69dccc9b3433aa2cd217aaa05a9bf2424
omralhaj036-collab <omralhaj036@gmail.com>
2026-08-07 19:22:04 +0300

    Update logger.py

--- app/core/logger.py
+++ app/core/logger.py
@@ -1,10 +1,25 @@
+from pathlib import Path
+
 from loguru import logger
-import sys
+
+from app.core.config import settings
+
+
+Path(settings.LOGS_PATH).mkdir(
+    parents=True,
+    exist_ok=True
+)
 
 logger.remove()
 
 logger.add(
-    sys.stdout,
-    level="INFO",
-    format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level}</level> | {message}"
+    settings.LOGS_PATH / "sadam_vox.log",
+    rotation="10 MB",
+    retention="30 days",
+    level="INFO"
+)
+
+logger.add(
+    lambda msg: print(msg, end=""),
+    level="INFO"
 )
